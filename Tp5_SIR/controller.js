9baea2dc0413c1a198c71076816e09f5e12a0b23
Unknown <Alexis.poince@gmail.com>
2018-03-15 01:40:18 +0100

    Update controller.js

--- Tp5_SIR/controller.js
+++ Tp5_SIR/controller.js
@@ -1,4 +1,3 @@
-
 var editingMode = { rect: 0, line: 1 };
 
 function Pencil(ctx, drawing, canvas) {
@@ -8,29 +7,43 @@ function Pencil(ctx, drawing, canvas) {
 	this.currentShape = 0;
 
 	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
-
+	new dnd(canvas, this);
 
 	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
 
-    this.onInteractionStart = function(dnd) {
+	this.onInteractionStart = function(dnd) {	
+		// Gestion couleurs et epaisseur
+		this.currLineWidth = spinnerWidth.value;
+		this.currColour = colour.value;
 
-		if(this.currEditingMode==0){
-			this.currentShape = new Rectangle(dnd.xinit - dnd.xfin , dnd.yinit - dnd.yfin , dnd.xinit, dnd.yinit , this.currLineWidth, this.currColour);
+		if($('#butRect')[0].checked){
+			this.currentShape = new Rectangle(dnd.xinit , dnd.yinit, 0, 0, this.currLineWidth, this.currColour);
 		}else{
-			this.currentShape = new Line(dnd.xinit, dnd.yinit, 0, 0, this.currLineWidth, this.currColour);
+			this.currentShape = new Line(dnd.xinit, dnd.yinit, dnd.xfin, dnd.yfin, this.currLineWidth, this.currColour);
 		}
-	}.bind(this)
+		this.currentShape.paint(ctx);
+	}.bind(this);
 
 	this.onInteractionUpdate = function(dnd){
-
-		
-
-	}.bind(this)
+		if ($('#butRect')[0].checked) {
+			this.currentShape.largeur = dnd.xfin - dnd.xinit;
+			this.currentShape.hauteur = dnd.yfin - dnd.yinit;
+			this.currentShape.epaisseur = this.currLineWidth;
+			this.currentShape.couleur = this.currColour;
+		}
+		else {
+			this.currentShape.xfin = dnd.xfin;
+			this.currentShape.yfin = dnd.yfin;
+		}
+		drawing.paint(ctx);
+		this.currentShape.paint(ctx);
+	}.bind(this);
 
 	this.onInteractionEnd = function(dnd){
-
-		
-	}.bind(this)
+		drawing.addForm(this.currentShape);
+		drawing.paint(ctx);
+		drawing.updateShapeList(this.currentShape);
+	}.bind(this);
 
 };
 
